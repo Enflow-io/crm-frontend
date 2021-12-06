3a0e0258730f9610b175fa06060957563570c9da
Enflow.io <konstantin@enflow.io>
2021-12-06 15:21:34 +0300

    non responsive

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,18 @@ import '../styles/andtstyles.less'
 import type { AppProps } from 'next/app'
 import * as Lockr from 'lockr'
 import {useRouter} from "next/router";
-
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
 
 
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      {/*<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />*/}
+      <meta content="width=1024" name="viewport" />
+    </Head>
+    <Component {...pageProps} />
+    </>
 }
 
 export default MyApp
