d8ea76c0c6dce357272d5d15ea8deff9408a4927
Enflow.io <konstantin@enflow.io>
2022-08-07 19:06:00 +0300

    logger page

---
+++ components/tables/LogsList.tsx
@@ -0,0 +1,58 @@
+import {Table, Button, Spin} from 'antd';
+import React, {useState} from "react";
+
+
+interface ObjectsListProps {
+    buildingsList: any[]
+    columns: { title: string, dataIndex: string }[]
+    onPageChanged?: (pageNumber: number) => void
+    onPageSizeChanged?: (pageNumber: number) => void
+    currentPage: number
+    totalItems: number
+    isDataLoading: boolean
+    onRowClick?: (id: any) => void
+}
+
+function LogsList(props: ObjectsListProps) {
+    const [loading, setLoading] = useState();
+    const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+
+
+    return (
+        <div>
+
+            <Table
+                scroll={{y: 'calc(100vh - 382px)'}}
+                columns={props.columns}
+                dataSource={props.buildingsList}
+                rowKey="id"
+                onRow={(record, rowIndex) => {
+                    return {
+                        onClick: event => {
+                            if (props.onRowClick) {
+                                props.onRowClick(record.id)
+                            }
+                        },
+                    };
+                }}
+                loading={{indicator: <div><Spin /></div>, spinning: props.isDataLoading}}
+                pagination={{
+                    total: props.totalItems,
+                    current: props.currentPage,
+                    onChange: (page, pageSize) => {
+                        console.log('current page: ', page)
+                        if (props.onPageChanged) {
+                            props.onPageChanged(page)
+                        }
+                        if (props.onPageSizeChanged) {
+                            props.onPageSizeChanged(pageSize)
+                        }
+
+                    }
+                }}
+            />
+        </div>
+    );
+}
+
+export default LogsList
