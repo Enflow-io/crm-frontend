1f369acc53ce50ade2ceb70695be3b10d45226c6
Enflow.io <konstantin@enflow.io>
2022-08-09 11:09:39 +0300

    block copying

--- effects/block.effects.ts
+++ effects/block.effects.ts
@@ -1,4 +1,4 @@
-import {createEffect} from "effector";
+import {createEffect, createEvent, createStore} from "effector";
 
 export const SubmitBlockForm = createEffect(async () => {
     return true
@@ -11,3 +11,23 @@ export const BlockCreated = createEffect(async () => {
 export const BlockUpdated = createEffect(async () => {
     return true
 })
+
+
+export const saveBlockToCopy = createEvent<any>();
+export const clearBlockToCopy = createEvent<any>();
+export const openBlockCreateModal = createEvent<any>();
+export const closeBlockCreateModal = createEvent<any>();
+export const $blockToCopyStore = createStore<any>({})
+    .on(saveBlockToCopy, (state, blockToCopy) => {
+        const clone = {...blockToCopy}
+        delete clone["createdAt"]
+        delete clone["updatedAt"]
+        delete clone["localId"]
+        return clone
+    })
+    .on(closeBlockCreateModal, (state, blockToCopy) => {
+        return {}
+    })
+    .on(clearBlockToCopy, (state, blockToCopy) => {
+        return {}
+    })
