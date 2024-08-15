546fcee15a3bb6f8ad4cf08c010e40b0a2beb853
kartograff <kartograff88@gmail.com>
2024-08-15 22:48:29 +0200

    Контрагенты

---
+++ pages/calendar.tsx
@@ -0,0 +1,11 @@
+
+import MainLayout from "../components/Layout/Layout"
+import CalendarComponent from "../components/Calendar/CalendarComponent";
+
+const CalendarPage = () => {
+    
+    return <MainLayout>
+       <CalendarComponent />
+    </MainLayout>
+}
+export default CalendarPage;
