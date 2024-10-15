2b037a9847c1e7ed35072c535c29c4da7b14754b
Aleksandr Kiryakov <kartograff88@gmail.com>
2024-10-15 22:31:15 +0200

    Поиск по айди ЦИАН

--- components/Competitors/OffersTable/OffersTable.tsx
+++ components/Competitors/OffersTable/OffersTable.tsx
@@ -1,19 +1,109 @@
-import React from "react";
-import {Table} from "antd";
+import React, {useRef, useState} from "react";
+import {Button, Input, Space, Table, TableColumnType} from "antd";
 import {IOffer} from "../../../interfaces/Competitors";
 import moment from "moment";
 import classes from "./OffersTable.module.scss";
+import {SearchOutlined} from "@ant-design/icons";
+import {FilterDropdownProps} from "antd/es/table/interface";
 
 type Props = {
     offers: IOffer[]
 }
+type DataIndex = keyof IOffer;
 const OffersTable = ({offers}: Props) => {
+    const [searchText, setSearchText] = useState('');
+    const [searchedColumn, setSearchedColumn] = useState('');
+    const searchInput = useRef<any>(null);
+    const handleSearch = (
+        selectedKeys: string[],
+        confirm: FilterDropdownProps['confirm'],
+        dataIndex: DataIndex,
+    ) => {
+        confirm();
+        setSearchText(selectedKeys[0]);
+        setSearchedColumn(dataIndex);
+    };
+
+    const handleReset = (clearFilters: () => void, confirm: FilterDropdownProps['confirm']) => {
+        clearFilters();
+        setSearchText('');
+        confirm();
+    };
+    const getColumnSearchProps = (dataIndex: DataIndex): TableColumnType<IOffer> => ({
+        // @ts-ignore
+        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+            <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+                <Input
+                    ref={searchInput}
+                    placeholder={`Search ${dataIndex}`}
+                    value={selectedKeys[0]}
+                    onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                    onPressEnter={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+                    style={{ marginBottom: 8, display: 'block' }}
+                />
+                <Space>
+                    <Button
+                        type="primary"
+                        onClick={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+                        icon={<SearchOutlined />}
+                        size="small"
+                        style={{ width: 90 }}
+                    >
+                        Search
+                    </Button>
+                    <Button
+                        onClick={() => clearFilters && handleReset(clearFilters, confirm)}
+                        size="small"
+                        style={{ width: 90 }}
+                    >
+                        Reset
+                    </Button>
+                    <Button
+                        type="link"
+                        size="small"
+                        onClick={() => {
+                            confirm({ closeDropdown: false });
+                            setSearchText((selectedKeys as string[])[0]);
+                            setSearchedColumn(dataIndex);
+                        }}
+                    >
+                        Filter
+                    </Button>
+                    <Button
+                        type="link"
+                        size="small"
+                        onClick={() => {
+                            close();
+                        }}
+                    >
+                        close
+                    </Button>
+                </Space>
+            </div>
+        ),
+        filterIcon: (filtered: boolean) => (
+            <SearchOutlined style={{ color: filtered ? '#1677ff' : undefined }} />
+        ),
+        onFilter: (value: any, record: any) => {
+            const req = record[dataIndex] ? record[dataIndex].toString() : null
+            return req ? req
+                .toLowerCase()
+                .includes((value as string).toLowerCase()) : false
+        },
+        // @ts-ignore
+        onFilterDropdownOpenChange: (visible: any) => {
+            if (visible) {
+                setTimeout(() => searchInput.current?.select(), 100);
+            }
+        },
+    });
     const columns = [
         {
             dataIndex: 'cianId',
             title: 'Cian Id',
             sorter: (a: IOffer, b: IOffer) => a.cianId - b.cianId,
-            render: (val: number, record: IOffer) => { return <a href={record.link} target="_blank" rel="noreferrer">{val}</a> }
+            render: (val: number, record: IOffer) => { return <a href={record.link} target="_blank" rel="noreferrer">{val}</a> },
+            ...getColumnSearchProps('cianId')
         },
         {
             dataIndex: 'type',
